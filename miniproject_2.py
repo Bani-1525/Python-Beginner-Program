60ac94ded4646722b2a03a956249f71e07c8035f
Bani Aggarwal <baniaggarwal015@gmail.com>
2025-08-01 01:49:16 +0530

    Add files via upload

---
+++ miniproject_2.py
@@ -0,0 +1,10 @@
+# To generate a random pasword
+import random 
+import string 
+
+pass_len = 12
+char_values = string.ascii_letters + string.digits + string.punctuation
+password= ""
+for i in range (pass_len):
+    password+= random.choice(char_values)
+print("Your password is: ", password)
